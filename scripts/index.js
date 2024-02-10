9d5cf64d15d88d1f5a38bccb2b4345d9df92a769
Muhammad Hanif (hanifm5) <hanifm5@coventry.ac.uk>
2024-02-10 17:48:34 +0000

    added emaail functionality WIP

--- scripts/index.js
+++ scripts/index.js
@@ -7,4 +7,21 @@ fetch("../components/navbar.html")
 	.then((data) => {
 		document.getElementById("navbar").innerHTML = data;
 	})
-	.catch((error) => console.error("Error loading the navbar:", error));
+	.catch((error) => console.error("error loading the navbar: ", error));
+
+function sendEmail(recipient, sender, message) {
+	var infoToSend = {
+		to_name: recipient,
+		from_name: sender,
+		message: message
+	};
+
+	emailjs.send("service_4p2b749", "template_xhuyici", infoToSend).then(
+		function (response) {
+			console.log("SUCCESS!", response.status, response.text);
+		},
+		function (error) {
+			console.log("FAILED...", error);
+		}
+	);
+}
